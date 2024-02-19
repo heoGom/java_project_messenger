eff06aaf765078b9f3f5e72f635b5c968901ffbf
heoGom <gjtjdwo1004@naver.com>
2024-02-19 18:05:15 +0900

    내꺼

--- _3rd project messenger/src/Membership.java
+++ _3rd project messenger/src/Membership.java
@@ -1,13 +1,16 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -15,6 +18,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -33,7 +37,7 @@ public class Membership extends JFrame {
 	private JButton picturebtn;
 	private JButton confirmbtn;
 	private JFrame frame;
-	private JLabel pictureLabel;
+	JLabel pictureLabel;
 	private JButton idDupbtn;
 	private JButton nickDupbtn;
 
@@ -71,10 +75,43 @@ public class Membership extends JFrame {
 					for (File file : selectedFiles) {
 						System.out.println("Selected File: " + file.getAbsolutePath());
 						displayImage(file.getAbsolutePath());
+						pictureLabel.setText("");
 						filePath = file.getAbsoluteFile();
 					}
 				}
 			}
+		});
+		pictureLabel.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+				fileChooser.setMultiSelectionEnabled(true);
+
+				int result = fileChooser.showOpenDialog(frame);
+
+				if (result == JFileChooser.APPROVE_OPTION) {
+					File[] selectedFiles = fileChooser.getSelectedFiles();
+					for (File file : selectedFiles) {
+						System.out.println("Selected File: " + file.getAbsolutePath());
+						displayImage(file.getAbsolutePath());
+						pictureLabel.setText("");
+						filePath = file.getAbsoluteFile();
+					}
+				}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				pictureLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				pictureLabel.setBorder(BorderFactory.createEmptyBorder());
+			}
+
 		});
 
 		confirmbtn.addActionListener(new ActionListener() {
@@ -91,15 +128,13 @@ public class Membership extends JFrame {
 						stmt.setString(2, password_pf2.getText());
 						stmt.setString(3, textField_3.getText());
 						stmt.setBytes(4, null);
-						
-
 
 						if (filePath != null) {
 							BufferedImage originalImage = ImageIO.read(filePath);
-		                    BufferedImage resizedImage = resizeImage(originalImage, 200, 200);
-		                    byte[] imageData = imageToByteArray(resizedImage);
+							BufferedImage resizedImage = resizeImage(originalImage, 150, 150);
+							byte[] imageData = imageToByteArray(resizedImage);
 							stmt.setBytes(4, imageData);
-						}else {
+						} else {
 							stmt.setBytes(4, null);
 
 						}
@@ -122,10 +157,12 @@ public class Membership extends JFrame {
 				isRightId = membershipdao.CheckId(id_tf.getText());
 				if (isRightId) {
 					JOptionPane.showMessageDialog(null, "사용가능한 아이디입니다.");
+					id_lbl.setText("사용가능");
 				} else {
 
 					JOptionPane.showMessageDialog(null, "사용불가한 아이디입니다.");
 					id_lbl.setText("중복된 아이디 입니다.");
+					id_lbl.setText("사용불가");
 				}
 
 			}
@@ -153,7 +190,7 @@ public class Membership extends JFrame {
 				String str = id_tf.getText();
 				boolean isValid = isValidIdPattern(str);
 				if (isValid) {
-					id_lbl.setText("사용가능");
+					id_lbl.setText("사용가능, 중복확인 필요");
 				} else {
 					id_lbl.setText("사용불가");
 				}
@@ -174,8 +211,23 @@ public class Membership extends JFrame {
 				boolean isValid = isValidPasswordPattern(password);
 				if (isValid) {
 					password_lbl.setText("사용가능");
-				} else {
+					char[] origin = password_pf.getPassword();
+					String sOrigin = new String(origin);
+					char[] copy = password_pf2.getPassword();
+					String sCopy = new String(copy);
+					if (sCopy.equals("")) {
+
+					} else {
+						if (sOrigin.equals(sCopy)) {
+							password_lbl2.setText("사용가능");
+						} else {
+							password_lbl2.setText("사용불가");
+						}
+					}
+				} else if (!isValid) {
 					password_lbl.setText("사용불가");
+					password_lbl2.setText("");
+
 				}
 
 			}
@@ -190,11 +242,17 @@ public class Membership extends JFrame {
 				String sOrigin = new String(origin);
 				char[] copy = password_pf2.getPassword();
 				String sCopy = new String(copy);
-				if (sOrigin.equals(sCopy)) {
-					password_lbl2.setText("사용가능");
+				if (password_lbl.getText().equals("사용가능")) {
+
+					if (sOrigin.equals(sCopy)) {
+						password_lbl2.setText("사용가능");
+					} else {
+						password_lbl2.setText("사용불가");
+					}
 				} else {
-					password_lbl2.setText("사용불가");
+					password_lbl2.setText("");
 				}
+
 			}
 		});
 		textField_3.addKeyListener(new KeyAdapter() {
@@ -204,7 +262,7 @@ public class Membership extends JFrame {
 				String str = textField_3.getText();
 				boolean isValid = isValidNickNamePattern(str);
 				if (isValid) {
-					nick_lbl.setText("사용가능");
+					nick_lbl.setText("사용가능, 중복확인 필요");
 				} else {
 					nick_lbl.setText("사용불가");
 				}
@@ -219,8 +277,14 @@ public class Membership extends JFrame {
 
 	private BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) {
 		Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_DEFAULT);
-		BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
-		resizedImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+		BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_ARGB);
+
+		Graphics2D g2 = resizedImage.createGraphics();
+		g2.setColor(Color.WHITE);
+		g2.fillRect(0, 0, targetWidth, targetHeight);
+		g2.drawImage(resultingImage, 0, 0, null);
+		g2.dispose();
+
 		return resizedImage;
 	}
 
@@ -239,11 +303,12 @@ public class Membership extends JFrame {
 				return;
 			}
 
-			BufferedImage image = ImageIO.read(file);
+			BufferedImage originalImage = ImageIO.read(file);
+			BufferedImage resizedImage = resizeImage(originalImage, 150, 150);
 
-			if (image != null) {
-				ImageIcon icon = new ImageIcon(image);
-				 pictureLabel.setIcon(icon);
+			if (resizedImage != null) {
+				ImageIcon icon = new ImageIcon(resizedImage);
+				pictureLabel.setIcon(icon);
 			} else {
 				JOptionPane.showMessageDialog(this, "이미지를 읽을 수 없습니다.", "에러", JOptionPane.ERROR_MESSAGE);
 			}
@@ -272,32 +337,34 @@ public class Membership extends JFrame {
 		getContentPane().setLayout(null);
 
 		pictureLabel = new JLabel();
-		pictureLabel.setText("임시로넣어둔");
-		pictureLabel.setBounds(12, 294, 200, 200);
+		pictureLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		pictureLabel.setText("\uD504\uB85C\uD544 \uC0AC\uC9C4 \uB4F1\uB85D");
+		pictureLabel.setBounds(111, 287, 150, 150);
 		getContentPane().add(pictureLabel);
 
 		id_tf = new JTextField();
-		id_tf.setBounds(128, 85, 116, 21);
+		id_tf.setBounds(128, 55, 116, 21);
 		getContentPane().add(id_tf);
 		id_tf.setColumns(10);
 
 		password_pf = new JPasswordField();
-		password_pf.setBounds(128, 141, 116, 21);
+		password_pf.setBounds(128, 111, 116, 21);
 		getContentPane().add(password_pf);
 		password_pf.setColumns(10);
 
 		password_pf2 = new JPasswordField();
-		password_pf2.setBounds(128, 195, 116, 21);
+		password_pf2.setBounds(128, 165, 116, 21);
 		getContentPane().add(password_pf2);
 		password_pf2.setColumns(10);
 
 		textField_3 = new JTextField();
-		textField_3.setBounds(128, 251, 116, 21);
+		textField_3.setBounds(128, 221, 116, 21);
 		getContentPane().add(textField_3);
 		textField_3.setColumns(10);
 
-		picturebtn = new JButton("프로필 사진등록");
-		picturebtn.setBounds(116, 310, 155, 36);
+		picturebtn = new JButton("\uC0AC\uC9C4 \uB4F1\uB85D");
+		picturebtn.setVisible(false);
+		picturebtn.setBounds(273, 330, 97, 23);
 		getContentPane().add(picturebtn);
 
 		confirmbtn = new JButton("확인");
@@ -305,47 +372,47 @@ public class Membership extends JFrame {
 		getContentPane().add(confirmbtn);
 
 		JLabel lblNewLabel = new JLabel("ID");
-		lblNewLabel.setBounds(41, 88, 57, 15);
+		lblNewLabel.setBounds(41, 58, 57, 15);
 		getContentPane().add(lblNewLabel);
 
 		JLabel lblNewLabel_1 = new JLabel("PW");
-		lblNewLabel_1.setBounds(41, 144, 57, 15);
+		lblNewLabel_1.setBounds(41, 114, 57, 15);
 		getContentPane().add(lblNewLabel_1);
 
 		JLabel lblNewLabel_2 = new JLabel("PW확인");
-		lblNewLabel_2.setBounds(41, 198, 57, 15);
+		lblNewLabel_2.setBounds(41, 168, 57, 15);
 		getContentPane().add(lblNewLabel_2);
 
 		JLabel lblNewLabel_3 = new JLabel("닉네임");
-		lblNewLabel_3.setBounds(41, 254, 57, 15);
+		lblNewLabel_3.setBounds(41, 224, 57, 15);
 		getContentPane().add(lblNewLabel_3);
 
 		id_lbl = new JLabel("2~10자 내외,특수문자불가");
 		id_lbl.setHorizontalAlignment(SwingConstants.CENTER);
-		id_lbl.setBounds(102, 116, 169, 15);
+		id_lbl.setBounds(102, 86, 169, 15);
 		getContentPane().add(id_lbl);
 
 		password_lbl = new JLabel("20자리 이하");
 		password_lbl.setHorizontalAlignment(SwingConstants.CENTER);
-		password_lbl.setBounds(128, 172, 116, 15);
+		password_lbl.setBounds(128, 142, 116, 15);
 		getContentPane().add(password_lbl);
 
 		password_lbl2 = new JLabel("");
 		password_lbl2.setHorizontalAlignment(SwingConstants.CENTER);
-		password_lbl2.setBounds(128, 226, 116, 15);
+		password_lbl2.setBounds(128, 196, 116, 15);
 		getContentPane().add(password_lbl2);
 
 		nick_lbl = new JLabel("\uD55C,\uC601, \uC22B\uC790, \uD2B9\uC218\uBB38\uC790 20\uC790 \uC774\uD558");
 		nick_lbl.setHorizontalAlignment(SwingConstants.CENTER);
-		nick_lbl.setBounds(94, 282, 185, 15);
+		nick_lbl.setBounds(94, 252, 185, 15);
 		getContentPane().add(nick_lbl);
 
 		idDupbtn = new JButton("중복확인");
-		idDupbtn.setBounds(273, 84, 97, 23);
+		idDupbtn.setBounds(273, 54, 97, 23);
 		getContentPane().add(idDupbtn);
 
 		nickDupbtn = new JButton("중복 확인");
-		nickDupbtn.setBounds(273, 250, 97, 23);
+		nickDupbtn.setBounds(273, 220, 97, 23);
 		getContentPane().add(nickDupbtn);
 
 	}
